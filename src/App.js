e09608111d9608f31a34335cda428864944023d4
milkbreadzee <riaelisabeths@gmail.com>
2022-12-26 16:16:31 +0530

    body

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar"
 import Header from './components/Header/Header';
+import Help from './components/Help/Help';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
+      <Help />
     </div>
   );
 }
---
+++ src/components/Help/Help.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import './Help.css'
+const Help = () => {
+  return (
+    <div>
+        <div className="box">
+            <div className="griditem">
+
+            <div className="grid1">
+                    <img src="https://theme.zdassets.com/theme_assets/2197739/aed6500f4da41c641f3de5d96247eb6654e9c386.png" alt="firstimg" />
+                        <div className="grid11">
+                            <h2>Using Abstract</h2>
+                            <p>Abstract lets you manage, version, and document your designs in one place.</p>
+                            <a href="/">Learn More →</a>
+                        </div>
+                </div>
+                
+                
+            </div>
+            <div className="griditem"> 
+            <div className="grid1">
+                <img src="https://theme.zdassets.com/theme_assets/2197739/91588b3d042df901d9b941ccbb57af4f151401c7.png" alt="2img" />
+                        <div className="grid11">
+                            <h2>Using Abstract</h2>
+                            <p>Abstract lets you manage, version, and document your designs in one place.</p>
+                            <a href="/">Learn More →</a>
+                        </div>
+                </div>
+            </div>
+            <div className="griditem"> 
+            <div className="grid1">
+                <img src="https://theme.zdassets.com/theme_assets/2197739/50d0db451c0e1e66e1caddd142986fcf6792ca05.png" alt="3img" />
+                        <div className="grid11">
+                            <h2>Using Abstract</h2>
+                            <p>Abstract lets you manage, version, and document your designs in one place.</p>
+                            <a href="/">Learn More →</a>
+                        </div>
+                </div>
+            </div>
+            <div className="griditem"> 
+            <div className="grid1">
+                <img src="https://theme.zdassets.com/theme_assets/2197739/3e82d0126e97d7b7c60a611d4e11b3c278d33743.png" alt="4img" />
+                        <div className="grid11">
+                            <h2>Using Abstract</h2>
+                            <p>Abstract lets you manage, version, and document your designs in one place.</p>
+                            <a href="/">Learn More →</a>
+                        </div>
+                </div>
+            </div>
+            <div className="griditem">
+            <div className="grid1">
+                <img src="https://theme.zdassets.com/theme_assets/2197739/7712592465ef5c7bf18f1c7cb16e19ab44eb81e3.png" alt="5img" />
+                        <div className="grid11">
+                            <h2>Using Abstract</h2>
+                            <p>Abstract lets you manage, version, and document your designs in one place.</p>
+                            <a href="/">Learn More →</a>
+                        </div>
+                </div>
+            </div>
+            <div className="griditem">
+                <div className="grid1">
+                    <img src="https://theme.zdassets.com/theme_assets/2197739/dcc25d18e224d12621d74e545cc8bdd1b5685ca8.png" alt="6img" />
+                        <div className="grid11">
+                            <h2>Using Abstract</h2>
+                            <p>Abstract lets you manage, version, and document your designs in one place.</p>
+                            <a href="/">Learn More →</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Help
